52cf855c30ebd99355bc930fca2684f9be86fce0
Fabian Andiel <fabian.andiel@yahoo.com>
2019-05-16 14:01:05 +0200

    Second extra

---
+++ Desktop/FSWD70-JS-DAY01-Fabian-Boris/FabianExtraExcercise/Exc1Fabian.js
@@ -0,0 +1,44 @@
+var Start = ['apple', 'banana', 'kiwi'];
+Start.splice(2,0,"orange","strawberrie");
+console.log(Start);
+// console.log(Start2);
+
+
+
+// Start.splice(index, 0, item);
+
+// var arr = [];
+// arr[0] = "Jani";
+// arr[1] = "Hege";
+// arr[2] = "Stale";
+// arr[3] = "Kai Jim";
+// arr[4] = "Borge";
+
+// console.log(arr.join());
+// arr.splice(2, 0, "Lene");
+// console.log(arr.join());
+
+
+
+
+
+// __________________________________________________________________________________
+// var startString ="Hey there, i am a javascript developer, and i live in vienna";
+// var slicedString= startString.slice(11,38);
+
+// var slicedStringArray = new  Array();
+// slicedStringArray = slicedString.split(" ");
+
+// slicedStringArray.shift();
+
+// slicedStringArray.unshift("I");
+// console.log(slicedStringArray);
+
+
+
+
+
+
+
+
+
